193c6c40e856f4937f03001a60cee3e084c62675
allhusein <95891486+allhusein@users.noreply.github.com>
2022-06-05 20:02:49 +0700

    Revert "Update home.blade.php"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Halaman Utama') }}</div>
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,46 +14,7 @@
                         </div>
                     @endif
 
-                    <div class="in-card" style="text-align: center">
-                        <h3>Our Special Product</h3>
-                        <div class="container">
-                            <div class="row">
-                              <div class="col-sm">
-                                <div class="card">
-                                    <img src="..." class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                              </div>
-                              <div class="col-sm">
-                                <div class="card">
-                                    <img src="..." class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                              </div>
-                              <div class="col-sm">
-                                <div class="card">
-                                    <img src="..." class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                              </div>
-                              <div class="col-sm">
-                                <div class="card">
-                                    <img src="..." class="card-img-top" alt="...">
-                                    <div class="card-body">
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    </div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        
-                    </div>
+                    {{ __('You are logged in!') }}
                 </div>
             </div>
         </div>
