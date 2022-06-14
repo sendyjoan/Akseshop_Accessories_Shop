49525e3417e21baf2bb63f3c69e1af325938a0fa
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-14 13:30:40 +0700

    Update Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'Name',
+        'Email',
+        'Password',
+        'Alamat',
+        'Telephone',
+        'Gambar',
     ];
 
     /**
