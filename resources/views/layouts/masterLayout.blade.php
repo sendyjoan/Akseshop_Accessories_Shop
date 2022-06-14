10d60cc2c6e7fa65528c31fe6261b1ba166eef99
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-14 21:19:15 +0700

    Update masterLayout.blade.php

--- resources/views/layouts/masterLayout.blade.php
+++ resources/views/layouts/masterLayout.blade.php
@@ -75,7 +75,7 @@
                 <div class="collapse navbar-collapse" id="navbar-menu">
                     <ul class="nav navbar-nav ml-auto" data-in="fadeInDown" data-out="fadeOutUp">
                         <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="about.html">About Us</a></li>
+                        <li class="nav-item"><a class="nav-link" href="">About Us</a></li>
                         <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
                         <li class="nav-item"><a class="nav-link" href="contact-us.html">Contact Us</a></li>
                         
