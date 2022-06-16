602efe1585abd16abd71085743a356f25d52cc14
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-16 22:08:23 +0700

    Repair Form Inpot Product

--- resources/views/Admin/Barang/createBarang.blade.php
+++ resources/views/Admin/Barang/createBarang.blade.php
@@ -7,25 +7,27 @@
         <div class="col-sm-12 col-xl-6 ">
             <div class="bg-secondary text-center  rounded p-4">
                 <h6 class="mb-4">Tambah Barang</h6>
+                <form action="/articles" method="post" enctype="multipart/formdata">
+                    @csrf
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput"  placeholder="Nama Barang">
+                        <input type="text" class="form-control" id="floatingInput" name="namabarang" placeholder="Nama Barang">
                         <label for="floatingInput">Nama Barang</label>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input type="number" class="form-control" id="floatingNumber"placeholder="Stock">
+                        <input type="number" class="form-control" id="floatingNumber" placeholder="Stock" name="stock">
                         <label for="floatingNumber">Stock</label>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingPrice" placeholder="Harga Barang">
+                        <input type="text" class="form-control" id="floatingPrice" placeholder="Harga Barang" name="harga">
                         <label for="floatingPrice">Harga Barang</label>
                     </div>
                     <div class="mb-3">
-                        <input class="form-control bg-dark" type="file" id="formFile">
+                        <input class="form-control bg-dark" type="file" id="formFile" name="gambar" required="required">
                     </div>
-                    <a class="btn btn-success mt-3" href="#">Simpan</a> 
-                
+                    <a class="btn btn-success mt-3" type="submit" name="submit">Simpan</a> 
+                </form>
             </div>
         </div>
         </div>
