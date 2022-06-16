3858b2d8a3f73f37c48f14f792756971a4c2c0b8
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-16 15:28:24 +0700

    Update ProductController.php
    Memberikan Data Product dari Database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,7 +14,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return view('Admin/Barang/barangIndex');
+        $products = Product::all();
+        // dd($products);
+        return view('Admin/Barang/barangIndex', compact('products'));
     }
 
     /**
