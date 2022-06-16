ed064ee0d074227b5cb81d3261613f22525fd660
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-16 15:28:59 +0700

    Update barangIndex.blade.php
    Mengatur view dari paramater database

--- resources/views/Admin/Barang/barangIndex.blade.php
+++ resources/views/Admin/Barang/barangIndex.blade.php
@@ -13,90 +13,23 @@
                         <table class="table text-start align-middle table-bordered table-hover mb-0">
                             <thead>
                                 <tr class="text-white">
-                                    <th scope="col"><input class="form-check-input" type="checkbox"></th>
-                                    <th scope="col">ID</th>
                                     <th scope="col">Nama Barang</th>
-                                    <th scope="col">Deskripsi</th>
                                     <th scope="col">Stock</th>
                                     <th scope="col">Harga</th>
                                     <th scope="col">Action</th>
-                                    
-
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01</td>
-                                    <td>Jam Tangan Rolex</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci a ea tenetur porro fugit consequuntur numquam quam quos. </td>
-                                    <td>10</td>
-                                    <td>15000</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Show</a>
-                                    <a class="btn btn-sm btn-primary" href="">Update</a>
-                                    <a class="btn btn-sm btn-primary" href="">Delete</a></td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01</td>
-                                    <td>Jam Tangan Rolex</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci a ea tenetur porro fugit consequuntur numquam quam quos. </td>
-                                    <td>10</td>
-                                    <td>15000</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Show</a>
-                                    <a class="btn btn-sm btn-primary" href="">Update</a>
-                                    <a class="btn btn-sm btn-primary" href="">Delete</a></td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01</td>
-                                    <td>Jam Tangan Rolex</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci a ea tenetur porro fugit consequuntur numquam quam quos. </td>
-                                    <td>10</td>
-                                    <td>15000</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Show</a>
-                                    <a class="btn btn-sm btn-primary" href="">Update</a>
-                                    <a class="btn btn-sm btn-primary" href="">Delete</a></td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01</td>
-                                    <td>Jam Tangan Rolex</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci a ea tenetur porro fugit consequuntur numquam quam quos. </td>
-                                    <td>10</td>
-                                    <td>15000</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Show</a>
-                                    <a class="btn btn-sm btn-primary" href="">Update</a>
-                                    <a class="btn btn-sm btn-primary" href="">Delete</a></td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01</td>
-                                    <td>Jam Tangan Rolex</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci a ea tenetur porro fugit consequuntur numquam quam quos. </td>
-                                    <td>10</td>
-                                    <td>15000</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Show</a>
-                                    <a class="btn btn-sm btn-primary" href="">Update</a>
-                                    <a class="btn btn-sm btn-primary" href="">Delete</a></td>
-                                    
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01</td>
-                                    <td>Jam Tangan Rolex</td>
-                                    <td>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci a ea tenetur porro fugit consequuntur numquam quam quos. </td>
-                                    <td>10</td>
-                                    <td>15000</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Show</a>
-                                    <a class="btn btn-sm btn-primary" href="">Update</a>
-                                    <a class="btn btn-sm btn-primary" href="">Delete</a></td>
-                                    
-                                </tr>
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <td>{{ $product->namaproduct}}</td>
+                                        <td>{{ $product->stock}}</td>
+                                        <td>{{ $product->harga}}</td>
+                                        <td><a class="btn btn-sm btn-primary" href="">Detail</a>
+                                        <a class="btn btn-sm btn-primary" href="">Update</a>
+                                        <a class="btn btn-sm btn-primary" href="">Delete</a></td>
+                                    </tr>
+                                @endforeach
                             </tbody>
                         </table>
                     </div>
