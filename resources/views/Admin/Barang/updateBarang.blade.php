89f09dc9176b49720a3ef0bb2932f426162b76f4
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-17 21:52:30 +0700

    Update updateBarang.blade.php
    Merge FE and BE

--- resources/views/Admin/Barang/updateBarang.blade.php
+++ resources/views/Admin/Barang/updateBarang.blade.php
@@ -6,38 +6,39 @@
 <div class="row g-4">
         <div class="col-sm-12 col-xl-6 ">
             <div class="bg-secondary text-center  rounded p-4">
-                <h6 class="mb-4">Update Barang</h6>
+                <form action="/dashboard/products" method="post" enctype="multipart/form-data">
+                    @csrf
+                    <h6 class="mb-4">Update Barang</h6>
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingInput"  placeholder="Nama Barang">
+                        <input type="text" class="form-control" name="namabarang" id="floatingInput"  placeholder="Nama Barang" value="{{ $product->namaproduct}}">
                         <label for="floatingInput">Nama Barang</label>
                     </div>
                     
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingNumber"placeholder="Stock">
+                        <input type="text" class="form-control" name="deskripsi" id="floatingNumber" placeholder="Deskripsi Barang" value="{{ $product->deskripsi}}">
                         <label for="floatingNumber">Deskripsi Barang</label>
                     </div>
 
                     <div class="form-floating mb-3">
-                        <input type="number" class="form-control" id="floatingNumber"placeholder="Stock">
+                        <input type="number" class="form-control" name="stock" id="floatingNumber" placeholder="Stock" value="{{ $product->stock}}">
                         <label for="floatingNumber">Stock</label>
                     </div>
         
-                        <select class="form-select mb-3" aria-label="Default select example">
-                                <option selected>Kategori</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
+                        <select name="kategori" class="form-select mb-3" aria-label="Default select example">
+                            @foreach ($categori as $ctg)
+                                <option value="{{ $ctg->idkategori }}" {{ $product->category_id_idkategori == $ctg->idkategori ? 'selected' : ''}}>{{ $ctg->namakategori}}</option>
+                            @endforeach
                         </select>
 
                     <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="floatingPrice" placeholder="Harga Barang">
+                        <input type="text" name="harga" class="form-control" id="floatingPrice" placeholder="Harga Barang" value="{{ $product->harga}}">
                         <label for="floatingPrice">Harga Barang</label>
                     </div>
                     <div class="mb-3">
-                        <input class="form-control bg-dark" type="file" id="formFile">
+                        <input class="form-control bg-dark" type="file" id="formFile" name="gambar" required="required">
                     </div>
-                    <a class="btn btn-success mt-3" href="#">Simpan</a> 
-                
+                    <button class="btn btn-success mt-3" type="submit" name="submit">Update</button> 
+                </form>
             </div>
         </div>
         </div>
