aefbcc176e04c1e71fc038235797d626c0792164
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-18 17:16:27 +0700

    Change Route (Not Fix)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,11 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $user = Auth::user();
+
+        // if ($user->role_id_idrole === 1) {
+        //     return redirect()->route('dashboard');
+        // }
         return view('home');
     }
 }
