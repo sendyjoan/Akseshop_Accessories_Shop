c472c43664032da6a42ceab5fd80cd0b04313099
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-21 15:59:14 +0700

    Update masterLayout.blade.php

--- resources/views/layouts/masterLayout.blade.php
+++ resources/views/layouts/masterLayout.blade.php
@@ -75,19 +75,13 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                               document.getElementById('logout-form').submit();">
-                                 {{ Auth::user()->name }}</a>
+                                 {{ Auth::user()->name }}
+                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
                                 </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Chart</a>
                             </li>
                         @endguest
                         
