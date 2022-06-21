f8e4db7da95042bc36584d00a371e6b709ed7f03
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-21 15:53:26 +0700

    Update Fitur Logout

--- resources/views/layouts/masterLayout.blade.php
+++ resources/views/layouts/masterLayout.blade.php
@@ -72,9 +72,10 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('logout') }}" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                              document.getElementById('logout-form').submit();">
+                                 {{ Auth::user()->name }}</a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
