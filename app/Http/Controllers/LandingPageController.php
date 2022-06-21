edfac080ba0fdc5a7fe65fd7b901db4a40933d43
Sendy Joan K <sendyjoan5@gmail.com>
2022-06-21 15:32:56 +0700

    Repair Landing Page

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 
 class LandingPageController extends Controller
@@ -17,39 +16,4 @@ class LandingPageController extends Controller
     public function aboutus(){
         return view('User/About');
     }
-
-    public function product(){
-        $products = Product::all();
-        return view('User/product', compact('products'));
-    }
-
-     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    public function show($id){
-        $product = Product::where('idproduct', $id)->first();
-        // dd($product);
-        return view('User/detailProduct', compact('product'));
-    }
-
-
-    public function index()
-    {
-         $user = Auth::user();
-
-         if ($user->role_id_idrole === 2) {
-             return redirect()->route('/');
-        }
-        // return view('User/index');
-    }
-
-
-    
 }
