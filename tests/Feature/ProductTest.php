988d882ce3438bda89c817fb0b7be4e6a6f6823c
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-09-20 19:24:15 +0700

    Create Test Code

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ProductTest extends TestCase
 {
@@ -13,17 +14,52 @@ class ProductTest extends TestCase
      *
      * @return void
      */
-    public function test_open_home_page()
+    public function test_open_product_page()
     {
-        $response = $this->get('/');
+        $response = $this->get('/dashboard/products');
 
         $response->assertStatus(200);
+        $response->assertSee('Daftar Barang');
     }
 
-    public function test_product_home_page(){
-        $response = $this->get('/');
+    public function test_one_product_on_product_page(){
+        $response = $this->get('/dashboard/products');
 
         $response->assertStatus(200);
         $response->assertSee('Cincin');
+    }
+
+    public function test_create_product(){
+        $product = Product::create([
+            'NamaProduct' => 'Test Create Product',
+            'Deskripsi' => 'Digunakan untuk test code',
+            'Stock' => '15',
+            'Harga' => '11000',
+            'Gambar' => 'Test Code',
+        ]);
+
+        $response = $this->get('/dashboard/products');
+        $response->assertStatus(200);
+        $response->assertSee('Test Create Product');
+        $response->assertSee('15');
+        $response->assertSee('11000');
+
+    }
+
+    public function test_edit_product(){
+        $product = Product::where('NamaProduct', 'Test Create Product')->update(['NamaProduct' => 'Test Edit Product', 'Stock' => 10]);
+        $response = $this->get('/dashboard/products');
+        $response->assertStatus(200);
+        $response->assertSee('Test Edit Product');
+        $response->assertSee('10');
+        $response->assertSee('11000');
+    }
+
+    public function test_delete_product(){
+        $product = Product::where('NamaProduct', 'Test Edit Product')->delete();
+
+        $response = $this->get('/dashboard/products');
+        $response->assertStatus(200);
+        $response->assertDontSee('Test Edit Product');
     }
 }
