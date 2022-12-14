428491ffa2d777c9f17e7ed238c920b503b51e1f
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-12-14 09:57:10 +0700

    wleee

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,8 @@ use Illuminate\Support\Facades\Artisan;
 // Route::get('/product', [LandingPageController::class, 'product']); //Menampilkan Halaman Product
 
 Route::get('/mysql', function () {
-    Artisan::call('migrate:fresh');
-    Artisan::call('db:seed');
+    Artisan::call('migrate:fresh', ['--force' => true]);
+    Artisan::call('db:seed', ['--force' => true]);
 });
 
 Route::prefix('dashboard')->group(function () {
