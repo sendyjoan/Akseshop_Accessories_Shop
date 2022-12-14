65d77250e8d8a04b1489a1940e90d30aeafa4deb
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-12-14 09:23:26 +0700

    Push

--- app/Http/Controllers/LandingPageController.php
+++ app/Http/Controllers/LandingPageController.php
@@ -9,7 +9,8 @@ use Illuminate\Http\Request;
 class LandingPageController extends Controller
 {
     public function landingpage(){
-        $products = Product::offset(0)->limit(4)->get();
+        // $products = Product::offset(0)->limit(4)->get();
+        $products = 1;
         return view('User/index', compact('products'));
     }
 
